a07b2377a80fe799a3d007cc44a2078064b2d34e
Uwe Gradenegger <uwe@gradenegger.eu>
2022-12-12 14:52:03 +0100

    Add ldif dumps

---
+++ TameMyCerts.IntegrationTests/Tests/GenericWebServer_emptyPolicy_Audit.Tests.ps1
@@ -0,0 +1,18 @@
+BeforeAll {
+
+    . "C:\IntegrationTests\Tests\lib\Init.ps1"
+
+}
+
+Describe 'GenericWebServer_emptyPolicy_Audit.Tests' {
+
+
+    It 'Given a request is not compliant but policy is in Audit mode, a certificate gets issued' {
+
+        $Csr = New-CertificateRequest -Subject "CN=www.intra.adcslabor.de" -KeyLength 2048
+        $Result = $Csr | Get-IssuedCertificate -ConfigString $ConfigString -CertificateTemplate "GenericWebServer_emptyPolicy_Audit"
+
+        $Result.Disposition | Should -Be $CertCli.CR_DISP_ISSUED
+        $Result.StatusCode | Should -Be $WinError.ERROR_SUCCESS
+    }
+}
