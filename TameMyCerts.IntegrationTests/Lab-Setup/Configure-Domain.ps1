02831d1029e3866a0f24e72d069eecf43bb5b6ec
Uwe Gradenegger <uwe@gradenegger.eu>
2022-12-12 15:28:02 +0100

    Fix User population script

--- TameMyCerts.IntegrationTests/Lab-Setup/Configure-Domain.ps1
+++ TameMyCerts.IntegrationTests/Lab-Setup/Configure-Domain.ps1
@@ -6,23 +6,6 @@
 [CmdletBinding()]
 param()
 
-New-Variable -Option Constant -Name BUILD_NUMBER_WINDOWS_2016 -Value 14393
-
-If ([int](Get-WmiObject -Class Win32_OperatingSystem).BuildNumber -lt $BUILD_NUMBER_WINDOWS_2016) {
-    Write-Error -Message "This must be run on Windows Server 2016 or newer! Aborting."
-    Return 
-}
-
-If (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Error -Message "This must be run as Administrator! Aborting."
-    Return
-}
-
-If (-not (Get-WmiObject -Class Win32_ComputerSystem).PartOfDomain) {
-    Write-Error "You must install the domain first!"
-    Return
-}
-
 $DomainName = "DC=tamemycerts-tests,DC=local"
 
 New-ADOrganizationalUnit `
@@ -32,7 +15,7 @@ New-ADOrganizationalUnit `
 
 $SecurePassword = "P@ssw0rd" | ConvertTo-SecureString -AsPlainText -Force
 
-(1..3) | ForEach-Object -Process {
+(1..5) | ForEach-Object -Process {
 
     New-ADUser `
         -SamAccountName "TestUser$($_)" `
